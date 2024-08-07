3e71dc232717a7f3791c4b0691699bdaed6e55b7
Miroslav Bajtoš <oss@bajtos.net>
2024-08-07 16:13:36 +0200

    feat: update allocator clients
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- run.sh
+++ run.sh
@@ -24,6 +24,10 @@ node scripts/build-spark-update-sql.js
 echo "** UPDATING THE PRODUCTION DATABASE **"
 psql "$DATABASE_URL" -f generated/update-spark-db.sql | tee generated/dbupdate.log
 
+echo "** Updating client-allocator mappings **"
+node scripts/update-allocator-clients.js | tee generated/allocator-update.log
+
 echo "** DONE **"
 grep "^DELETE" < generated/dbupdate.log | awk '{s+=$2} END {print "Deleted: " s}'
 grep "^INSERT" < generated/dbupdate.log | awk '{s+=$3} END {print "Added: " s}'
+tail -1 generated/allocator-update.log
