7b83e9ffb8724d8d490b7a2aabef6e404169b353
Miroslav Bajtoš <oss@bajtos.net>
2024-12-16 19:16:54 +0100

    feat: improve final log msg header
    Signed-off-by: Miroslav Bajtoš <oss@bajtos.net>

--- run.sh
+++ run.sh
@@ -27,7 +27,7 @@ psql "$DATABASE_URL" -f generated/update-spark-db.sql | tee generated/dbupdate.l
 echo "** Updating client-allocator mappings **"
 node scripts/update-allocator-clients.js | tee generated/allocator-update.log
 
-echo "** DONE **"
+echo "** FINISHED INGEST OF f05 DEALS**"
 grep "^DELETE" < generated/dbupdate.log | awk '{s+=$2} END {print "Deleted: " s}'
 grep "^INSERT" < generated/dbupdate.log | awk '{s+=$3} END {print "Added: " s}'
 tail -1 generated/allocator-update.log
